cbd60753e755e1105c7a81ce90c2a4c4e95927c9
NicoloGulotta <Gulottanico1998@gmail.com>
2024-04-04 12:32:36 +0200

    Ripasso >React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { } from 'react-icons/fa';
+import ComponenteTest from "./components/ComponenteTest";
+import { Button } from "react-bootstrap";
 
 function App() {
+  const alerta = () => {
+    alert('ma che cazzio faccio')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={alerta}> toccami</Button>
+      <ComponenteTest name='' />
     </div>
   );
 }
---
+++ src/components/ComponenteTest.jsx
@@ -0,0 +1,5 @@
+export default function componenteTest(props) {
+  return (
+    props.name
+  )
+}
